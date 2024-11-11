60ffc74c0a162f901eec1736e00f59c7a49fbbe2
DouglasDavila <douglasdavila404@gmail.com>
2024-11-11 16:50:11 -0300

    Implementação do primeiro modelo de banco de dados

--- user/cadastro.php
+++ user/cadastro.php
@@ -24,40 +24,48 @@ session_start();
             </div>
             <div class="form-side">
                 <h1>Criar Conta</h1>
-                <form action="process_cadastro.php" method="POST" autocomplete="off">
+                <form action="user.php" method="POST" autocomplete="off">
                     <div class="input-group">
-                        <input type="email" 
-                               name="email" 
-                               id="email" 
-                               placeholder="Email" 
-                               required 
-                               pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+                        <input type="text"
+                            name="nome"
+                            id="nome"
+                            placeholder="Nome"
+                            required
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+                    </div>
+                    <div class="input-group">
+                        <input type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Email"
+                            required
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
                     </div>
                     <div class="input-group">
                         <div class="password-container">
-                            <input type="password" 
-                                   name="senha" 
-                                   id="senha" 
-                                   placeholder="Senha" 
-                                   required 
-                                   minlength="8" 
-                                   maxlength="72">
+                            <input type="password"
+                                name="senha"
+                                id="senha"
+                                placeholder="Senha"
+                                required
+                                minlength="8"
+                                maxlength="72">
                             <i class="far fa-eye" id="togglePassword" aria-hidden="true"></i>
                         </div>
                     </div>
                     <div class="input-group">
                         <div class="password-container">
-                            <input type="password" 
-                                   name="confirmar_senha" 
-                                   id="confirmar_senha" 
-                                   placeholder="Confirmar senha" 
-                                   required 
-                                   minlength="8" 
-                                   maxlength="72">
+                            <input type="password"
+                                name="confirm"
+                                id="confirm"
+                                placeholder="Confirmar senha"
+                                required
+                                minlength="8"
+                                maxlength="72">
                             <i class="far fa-eye" id="toggleConfirmPassword" aria-hidden="true"></i>
                         </div>
                     </div>
-                    <button type="submit" class="btn-primary">Criar</button>
+                    <button type="submit" class="btn-primary" value="Salvar" name="acao">Criar</button>
                 </form>
             </div>
         </div>
